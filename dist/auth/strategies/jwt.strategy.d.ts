1fa3364896654c6d5d1fe1dc9a3bdbbeb9f3197c
TarundeepJoshi <mailforwork884@gmail.com>
2026-04-09 16:04:48 +0530

    Refactor code structure for improved readability and maintainability

--- dist/auth/strategies/jwt.strategy.d.ts
+++ dist/auth/strategies/jwt.strategy.d.ts
@@ -1,12 +1,12 @@
 import { Strategy } from 'passport-jwt';
-declare const AccessTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+declare const AccessTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class AccessTokenStrategy extends AccessTokenStrategy_base {
     constructor();
     validate(payload: any): any;
 }
-declare const RefreshTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithRequest] | [opt: import("passport-jwt").StrategyOptionsWithoutRequest]) => Strategy & {
+declare const RefreshTokenStrategy_base: new (...args: [opt: import("passport-jwt").StrategyOptionsWithoutRequest] | [opt: import("passport-jwt").StrategyOptionsWithRequest]) => Strategy & {
     validate(...args: any[]): unknown;
 };
 export declare class RefreshTokenStrategy extends RefreshTokenStrategy_base {
